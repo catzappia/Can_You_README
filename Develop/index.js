703214c68d000427adfa1fb0a44a98027993b198
Catherine Zappia <catzappia@aol.com>
2024-08-13 22:37:57 -0400

    Update index.js v01.03
    created function to initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,12 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      writeToFile('README.md', generateMarkdown(answers));
+    });
+}
 
 // Function call to initialize app
 init();
